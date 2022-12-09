a0ec4e2c01e3f42355efcb76bb824f9adfc69398
mshcherbakov-godaddy <112399112+mshcherbakov-godaddy@users.noreply.github.com>
2022-12-09 11:12:43 +0300

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+L
+.idea/codeStyles/Project.xml,6/e/6ecd6000a7b6f4a2884412ff19f74193ed089648
+@
+src/t2/Main.java,0/1/01d6ea28c4a34e9a52bc3563dc0b68b16474ed99
---
+++ src/t2/Main.java
@@ -0,0 +1,14 @@
+package t2;
+
+import java.util.LinkedHashMap;
+
+public class Main {
+  public static void main(String[] args) {
+    var textToTest = "This is an example of a long text. Try to find here the first non-repeated character.";
+    var map = new LinkedHashMap<Character, Integer>();
+    textToTest.chars().forEach(value -> map.compute((char) value, (k, v) -> v == null ? 1 : ++v));
+    map.entrySet().stream()
+        .filter(entry-> entry.getValue().equals(1)).findFirst()
+        .ifPresent((characterIntegerEntry -> System.out.println(characterIntegerEntry.getKey())));
+  }
+}
